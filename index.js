78d72367d92c2d7aac3e8ea99a794b936b0ad572
Nama <damayanti.nama@sunrise.net>
2023-08-21 18:53:28 +0530

    ep3 create schema and model

--- index.js
+++ index.js
@@ -3,3 +3,31 @@ const mongoose = require("mongoose")
 mongoose.connect('mongodb://localhost/playgroup')
 .then(() => console.log('DB connected'))
 .catch((error) => console.log('Not connected'))
+
+
+const courseSchema = new mongoose.Schema({
+    name:String,
+    author:String,
+    tags: [String],
+    date:{type:Date,default:Date.now},
+    isPublish:Boolean
+});
+
+async function createCourse(){
+    const Course = mongoose.model('Course',courseSchema)
+    const course = new Course({
+        name:'Express Course',
+        author:'NAMA',
+        tags: ['Node','express'],
+        isPublish:true
+    });
+
+    const result =  await course.save();
+    console.log(result);
+}
+createCourse()
+
+
+
+
+
