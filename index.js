60c74273624dcf520da67804ed64d953562f0bd8
Nama <damayanti.nama@sunrise.net>
2023-08-22 17:50:57 +0530

    ep5 operators

--- index.js
+++ index.js
@@ -10,17 +10,19 @@ const courseSchema = new mongoose.Schema({
     author:String,
     tags: [String],
     date:{type:Date,default:Date.now},
-    isPublish:Boolean
+    isPublish:Boolean,
+    price:Number
 });
 
 const Course = mongoose.model('Course',courseSchema)
 
 async function createCourse(){    
     const course = new Course({
-        name:'Node Course',
+        name:'Frontend',
         author:'DAMAYANTI',
         tags: ['Node','express'],
-        isPublish:false
+        isPublish:false,
+        price:50
     });
     const result =  await course.save();
     console.log(result);
@@ -30,10 +32,9 @@ async function getCourse(){
                     //.find({isPublish:false,author:'NAMA'})
                     //.find().limit(1)
                     //.find().limit(5).sort({name:-1}).select({name:1,tags:1})
-                    .find().limit(5).sort({name:-1}).count()
+                    .find().or([{author:'NAMA'},{isPulish:true}]).limit(5).sort({name:-1})
     console.log(course)
 }
-
 
 
 
